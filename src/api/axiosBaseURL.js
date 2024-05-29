066204f4609bea4c3777c99260ff7cf1ae94a86e
jubayer2023 <jubayersarker400@gmail.com>
2024-05-29 22:35:40 +0600

    again another changed

--- src/api/axiosBaseURL.js
+++ src/api/axiosBaseURL.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import { clearCookie } from "./auth";
 
 export const axiosPublic = axios.create({
-    baseURL: `https://blood-donation-server-teal.vercel.app`,
+    baseURL: `${import.meta.env.VITE_AXIOS_BASE_URL}`,
 });
 export const axiosSecure = axios.create({
-    baseURL: `https://blood-donation-server-teal.vercel.app`,
+    baseURL: `${import.meta.env.VITE_AXIOS_BASE_URL}`,
     withCredentials: true,
 });
 
